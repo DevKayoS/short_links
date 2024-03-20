2028dee1dd469e2ab4a19ffed02aa85cbb1f390c
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-03-20 11:05:49 -0300

    Realizado a criação de rotas de criacao e visualizacao dos links encurtados

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,68 @@
 import fastify from "fastify";
+import {z} from "zod"
+import {sql} from "./lib/postgres"
+import postgres from "postgres";
+
 
 const app = fastify()
 
 
-app.get('/teste', ()=> {
-  return 'Hello World'
+app.get('/:code', async (request, reply)=> {
+  const {code} = z.object({
+    code: z.string().min(3),
+  }).parse(request.params)
+
+  const result = await sql/*sql*/`
+    SELECT id, original_url
+    FROM short_links
+    WHERE short_links.code = ${code}
+  `
+   const link = result[0]
+
+   if(result.length === 0){
+    return reply.status(400).send({message: 'Link not found'})
+   }
+
+   return reply.redirect(301, link.original_url)
+})
+
+app.get('/api/links', async () => {
+  const result = await sql/*sql*/`
+    SELECT *
+    FROM short_links
+    ORDER BY created_at DESC
+  `
+
+  return result
+})
+
+app.post('/api/links', async (request, reply )=> {
+  const {code, url} = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  }).parse(request.body)
+
+  try {
+    const result = await sql/*sql*/`
+    INSERT INTO short_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNINg id
+  ` 
+
+  const link =result[0]
+
+  return reply.status(201).send({shortLinkId: link.id})
+  } catch (err) {
+    if (err instanceof postgres.PostgresError) {
+      if(err.code === '23505'){
+        return reply.status(400).send({message: "Duplicated Code"})
+      }
+    }
+
+    console.log(err)
+
+    return reply.status(500).send({message: "Internal Error"})
+  }
 })
 
 app.listen({
